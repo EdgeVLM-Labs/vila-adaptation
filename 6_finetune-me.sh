4429d1d74f79c3f0b39aaac6029b7a51dc689da2
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-12-03 10:16:42 +0530

    Add WANDB project and name exports for finetuning script

--- 6_finetune-me.sh
+++ 6_finetune-me.sh
@@ -4,6 +4,9 @@ export DEFAULT_GLOBAL_TRAIN_BATCH_SIZE=64
 export DEFAULT_GRADIENT_ACCUMULATION_STEPS=4
 export DEFAULT_GPUS_PER_NODE=1
 
+export WANDB_PROJECT="VILA"
+export WANDB_NAME="finetune-vila1.5-3b-qved"
+
 bash scripts/NVILA-Lite/sft.sh \
     Efficient-Large-Model/VILA1.5-3b \
     QVED-dataset
